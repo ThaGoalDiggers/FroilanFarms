4fbc11f4f5e27fc8895b37473450dc156c509e09
TroyCarroll48 <troycarroll48@yahoo.com>
2018-11-15 10:30:43 -0500

    uodated

---
+++ src/test/java/pojotest/CropRowTest.java
@@ -0,0 +1,18 @@
+package pojotest;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class CropRowTest {
+
+    @Test
+    public ArrayList<Crop> getPlantedCrops(){
+
+    }
+
+    @Test
+    public void setPlantedCrops(ArrayList<Crop> plantedCrops){
+
+    }
+}
---
+++ src/test/java/pojotest/StablesTest.java
@@ -0,0 +1,19 @@
+package pojotest;
+
+import org.junit.Test;
+import sun.java2d.pipe.hw.AccelTypedVolatileImage;
+
+import java.util.ArrayList;
+
+public class StablesTest {
+
+    @Test
+    public ArrayList<Horse> getHorseTest(){
+
+    }
+
+    @Test
+    public void setHorseTest(ArrayList<Horse> horses){
+
+    }
+}
